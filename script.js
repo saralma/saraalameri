cbc8f6b1dcddfbbb89363e4a9ac446be8727e424
saralma <158991084+saralma@users.noreply.github.com>
2024-05-06 07:30:26 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,124 @@
+$(function() {
+
+    $('.navbar-toggle').click(function() {
+        $(this).toggleClass('act');
+            if($(this).hasClass('act')) {
+                $('.main-menu').addClass('act');
+            }
+            else {
+                $('.main-menu').removeClass('act');
+            }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 10
+    });
+
+	/* Progress bar */
+    var $section = $('.section-skills');
+    function loadDaBars() {
+	    $('.progress .progress-bar').progressbar({
+	        transition_delay: 500
+	    });
+    }
+    
+    $(document).bind('scroll', function(ev) {
+        var scrollOffset = $(document).scrollTop();
+        var containerOffset = $section.offset().top - window.innerHeight;
+        if (scrollOffset > containerOffset) {
+            loadDaBars();
+            // unbind event not to load scrolsl again
+            $(document).unbind('scroll');
+        }
+    });
+
+    /* Counters  */
+    if ($(".section-counters .start").length>0) {
+        $(".section-counters .start").each(function() {
+            var stat_item = $(this),
+            offset = stat_item.offset().top;
+            $(window).scroll(function() {
+                if($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+	// another custom callback for counting to infinity
+	$('#infinity').data('countToOptions', {
+		onComplete: function (value) {
+		  count.call(this, {
+		    from: value,
+		    to: value + 1
+		  });
+		}
+	});
+
+	$('#infinity').each(count);
+
+	function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+    // Navigation overlay
+    var s = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: false,
+            mobileDeceleration: 0.004,
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+    });
+    
+});
+
+function typeWriter() {
+    console.log("Typing", i); // Log the current position
+    if (i < typewriterText.length) {
+        container.innerHTML += typewriterText.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var typewriterText = "I am Sara Alameri";
+    var container = document.getElementById('typewriter');
+    var i = 0;
+    var speed = 150; // Typing speed in milliseconds
+
+    function typeWriter() {
+        if (i < typewriterText.length) {
+            container.innerHTML += typewriterText.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        } else {
+            // Once the text is fully displayed, start over
+            setTimeout(function() {
+                container.innerHTML = ""; // Clear the text
+                i = 0; // Reset the counter
+                typeWriter(); // Start the typewriter effect again
+            }, 4000); // Wait 2 seconds before starting over
+        }
+    }
+
+    typeWriter();
+});
+
+
